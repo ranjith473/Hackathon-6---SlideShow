737f3343766ae01b79063f991e667c95bd33db1a
ranjith473 <74918552+ranjith473@users.noreply.github.com>
2021-02-12 22:25:33 +0530

    coded

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
+import slides from "../data";
 
-const App = () => {
+const App = (props) => {
   const [index, setIndex] = useState(0);
   return (
     <div>
@@ -13,6 +14,24 @@ const App = () => {
         onClick={() => setIndex(0)}
       >
         Restart
+      </button>
+      <button
+        data-testid="button-prev"
+        disabled={index === 0}
+        onClick={() => {
+          if (index > 0) setIndex(index - 1);
+        }}
+      >
+        Prev
+      </button>
+      <button
+        data-testid="button-next"
+        disabled={index === props.slides.length - 1}
+        onClick={() => {
+          if (index < props.slides.length - 1) setIndex(index + 1);
+        }}
+      >
+        Next
       </button>
     </div>
   );
