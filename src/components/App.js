ea1808123b4c2d379954a04ac8453ed86be4a5b8
ranjith473 <74918552+ranjith473@users.noreply.github.com>
2021-02-12 21:43:12 +0530

    code

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,20 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
+  const [index, setIndex] = useState(0);
   return (
-    <></>
-  )
-}
+    <div>
+      <h1 data-testid="title">{props.slides[index].title}</h1>
+      <p data-testid="text">{props.slides[index].text}</p>
+      <button
+        data-testid="button-restart"
+        disabled={index === 0}
+        onClick={() => setIndex(0)}
+      >
+        Restart
+      </button>
+    );
+  };
 
 
 export default App;
